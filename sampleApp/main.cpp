c6d3e47ec994ac1d938cc5fe3634b2b49a715fed
Erik Arthur <erikarthur@hotmail.com>
2013-09-09 06:00:43 -0500

    Initial Commit

---
+++ sampleApp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  sampleApp
+//
+//  Created by erik on 9/9/13.
+//  Copyright (c) 2013 Erik Arthur. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
